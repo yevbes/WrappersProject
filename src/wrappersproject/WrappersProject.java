fd3553c42d89f32bb914920dbeab17aba7eaa207
Yevgeniy Bespal <yevbes@yevbes.com>
2018-11-20 19:11:22 +0100

    Proyecto inicial

---
+++ src/wrappersproject/WrappersProject.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package wrappersproject;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ *
+ * @author yevbes
+ */
+public class WrappersProject {
+    
+    private static WebDriver driver= null;
+    
+    public static void Firefox()
+    {
+        String exePath = "/home/yevbes/NetBeansProjects/WrappersProject/geckodriver/geckodriver";
+        System.setProperty("webdriver.gecko.driver", exePath);
+        driver = new FirefoxDriver();
+        driver.get("http://www.amazon.es");
+        driver.manage().window().maximize();
+    }
+
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Firefox();
+    }
+    
+}
